6174336a7b437c7c11eb7f067b90bf0ed6ed11a2
Samuel Littley <samuel.littley@toastwaffle.com>
2018-02-11 15:58:28 +0000

    Add jerome setup

---
+++ .xmonad/jerome-start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+xset +dpms
+xset dpms 0 0 900
+
+# Startup apps
+if [ -x /usr/bin/udiskie ] ; then
+    /usr/bin/udiskie &
+fi
+
+if [ -x /opt/dropbox/dropbox ] ; then
+	/opt/dropbox/dropbox start &
+fi
+
+xsetroot -cursor_name left_ptr &
+
+setxkbmap gb &
+
+feh --bg-tile /home/samuel/.backgrounds/1920x1080.png
+
+stalonetray --background '#444444' --geometry '5x1+1820' --icon-size 20 --icon-gravity NE --max-geometry '96x1' &
